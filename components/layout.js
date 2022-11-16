29f4bd0505a2562675b7047e7f8dedb3658cf103
Cora Dale <cora.nightsong@gmail.com>
2022-11-17 10:36:23 +1300

    change name

--- components/layout.js
+++ components/layout.js
@@ -6,8 +6,8 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Heyy~';
-export const siteTitle = 'Heyy';
+const name = 'From Cora';
+export const siteTitle = 'test site';
 
 export default function Layout({ children, home }) {
 	return (
